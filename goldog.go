8fc2ca49068ab6c1e9c1ffa8a50bc520bcc52a43
Abhishek Keshri <keshri.abhishek63@gmail.com>
2020-05-31 04:54:47 +0530

    Add display method

--- goldog.go
+++ goldog.go
@@ -1,10 +1,21 @@
 package main
 
-import "math"
+import (
+	"fmt"
+	"math"
+)
 
 func colorize(i int) (int, int, int) {
 	var f = 0.1
 	return int(math.Sin(f*float64(i)+0)*127 + 128),
 		int(math.Sin(f*float64(i)+2*math.Pi/3)*127 + 128),
 		int(math.Sin(f*float64(i)+4*math.Pi/3)*127 + 128)
+}
+
+func display(text []rune) {
+	for j := 0; j < len(text); j++ {
+		r, g, b := colorize(j)
+		fmt.Printf("\033[38;2;%d;%d;%dm%c\033[0m", r, g, b, text[j])
+	}
+	fmt.Println()
 }
