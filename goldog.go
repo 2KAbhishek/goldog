a32b1a770ef892ba3c7f7cfc4c70ce8c2213fac6
Abhishek Keshri <keshri.abhishek63@gmail.com>
2020-05-31 04:37:50 +0530

    Add colorize method

--- goldog.go
+++ goldog.go
@@ -1,1 +1,10 @@
 package main
+
+import "math"
+
+func colorize(i int) (int, int, int) {
+	var f = 0.1
+	return int(math.Sin(f*float64(i)+0)*127 + 128),
+		int(math.Sin(f*float64(i)+2*math.Pi/3)*127 + 128),
+		int(math.Sin(f*float64(i)+4*math.Pi/3)*127 + 128)
+}
